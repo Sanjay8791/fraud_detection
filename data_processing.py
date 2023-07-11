57bb1d2dc759e0697c06500c90f082286b8b9ce9
Sanjay8791 <sanjaykatti87@gmail.com>
2023-07-11 20:19:12 +0530

    Correcting all functions

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,24 @@
 
 print("adding all the data processing code related to fraud detection")
 
+a = 5
+b = 7
+
+
+def sum(a,b):
+    c = a+b
+    return c
+
+print ("Added a summation function")
+
+def multiplication(a,b):
+    d = a*b
+    return d
+
+print ("Added a multiplication function")
+
+def devision (a,b):
+    e = a/b
+    return e
+
+print ("Added a modulus function")
